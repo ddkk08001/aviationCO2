36431da6ca3ddedd66fc1f970c6fc0174cc63e9a
ddkk08 <79787819+ddkk08001@users.noreply.github.com>
2024-12-11 09:56:19 +0100

    Update aviationCO2.py

--- aviationCO2.py
+++ aviationCO2.py
@@ -20,4 +20,34 @@ while True:
         os.system('clear')
         print_red_background("Error: Flying time must be valid number. Please enter a positive integer (WITHOUT UNITS).")
 
+def calculateB738Fuel(timeFlying):
+    return (13.75 * timeFlying)
+
+def calculateA320Fuel(timeFlying):
+    return (13.5 * timeFlying)
+
+def calculateB789Fuel(timeFlying):
+    return (18.92 * timeFlying)
+
+def calculateB77WFuel(timeFlying):
+    return (20.38 * timeFlying)
+
+def calculateB738CO2(timeFlying):
+    return (3.16 * calculateA320Fuel(timeFlying))
+
+def calculateA320CO2(timeFlying):
+    return (3.16 * calculateA320Fuel(timeFlying))
+
+def calculateB789CO2(timeFlying):
+    return (3.16 * calculateB789Fuel(timeFlying))
+
+def calculateB77WCO2(timeFlying):
+    return (3.16 * calculateB789Fuel(timeFlying))
+
 print (timeFlying)
+
+print(calculateB789Fuel(timeFlying), "kg/passenger")
+print(calculateB789CO2(timeFlying), "kg/passenger")
+
+
+#1
