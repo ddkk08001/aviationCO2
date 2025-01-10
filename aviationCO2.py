255df5ea88a393c9eef6547f4f1dfc595b2778fc
ddkk08 <79787819+ddkk08001@users.noreply.github.com>
2025-01-10 20:55:50 +0100

    Update aviationCO2.py

--- aviationCO2.py
+++ aviationCO2.py
@@ -67,14 +67,15 @@ fig1, ax1 = plt.subplots()
 
 color = 'tab:red'
 ax1.set_xlabel('Aircraft')
-ax1.set_ylabel('Fuel (kg/passenger)', color=color)
+ax1.set_ylabel('Fuel (kg/passenger)')
 ax1.bar(aircraft, fuel, color=color)
 ax1.tick_params(axis='y', labelcolor=color)
 ax1.set_title('Fuel Burned and CO2 Emissions per Passenger for Different Aircrafts')
 
 fig2, ax2 = plt.subplots()
 color = 'tab:blue'
-ax2.set_ylabel('CO2 (kg/passenger)', color=color)
+ax2.set_xlabel('Aircraft')
+ax2.set_ylabel('CO2 (kg/passenger)')
 ax2.bar(aircraft, co2, color=color)
 ax2.tick_params(axis='y', labelcolor=color)
 ax2.set_title('Fuel Burned and CO2 Emissions per Passenger for Different Aircrafts')
